1c253def3be4ba09140f88a6fde15326cb7e2ae5
rober <rober@DESKTOP-MV1AQ7L>
2023-01-17 15:09:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISM_4300_First_Deliverable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            string stg = "Hello, World!";
+            Console.WriteLine("Solution (2): " + stg);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + userName);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter the user's age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + age);
+
+
+        }
+    }
+}
+
